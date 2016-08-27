38d7d9a01ca7ea8468012c4da7d0d7c27258bf76
刘杨 <foamliu@yeah.net>
2016-08-28 00:31:22 +0800

    integrate DJI SDK

--- Android/app/src/main/java/cc/stargroup/dronexp/android/MainActivity.java
+++ Android/app/src/main/java/cc/stargroup/dronexp/android/MainActivity.java
@@ -2,6 +2,7 @@ package cc.stargroup.dronexp.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import dji.sdk.SDKManager.DJISDKManager;
 
 public class MainActivity extends AppCompatActivity {
 
