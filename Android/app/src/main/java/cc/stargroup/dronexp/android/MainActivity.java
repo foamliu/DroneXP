253c769db2738ecb28c611232424b8b059102b1d
刘杨 <foamliu@yeah.net>
2016-08-28 10:06:22 +0800

    android app呈现视频内容

--- Android/app/src/main/java/cc/stargroup/dronexp/android/MainActivity.java
+++ Android/app/src/main/java/cc/stargroup/dronexp/android/MainActivity.java
@@ -1,14 +1,146 @@
 package cc.stargroup.dronexp.android;
 
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import dji.sdk.SDKManager.DJISDKManager;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import dji.sdk.Camera.DJICamera;
+import dji.sdk.Codec.DJICodecManager;
+import dji.sdk.SDKManager.DJISDKManager;
+import dji.sdk.base.DJIBaseProduct;
+
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
+
+    private static final String TAG = MainActivity.class.getName();
+    protected DJICamera.CameraReceivedVideoDataCallback mReceivedVideoDataCallBack = null;
+
+    // Codec for video live view
+    protected DJICodecManager mCodecManager = null;
+
+    protected TextureView mVideoSurface = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        initUI();
+
+        // The callback for receiving the raw H264 video data for camera live view
+        mReceivedVideoDataCallBack = new DJICamera.CameraReceivedVideoDataCallback() {
+            @Override
+            public void onResult(byte[] videoBuffer, int size) {
+                if(mCodecManager != null){
+                    // Send the raw H264 video data to codec manager for decoding
+                    mCodecManager.sendDataToDecoder(videoBuffer, size);
+                }else {
+                    Log.e(TAG, "mCodecManager is null");
+                }
+            }
+        };
     }
+
+    protected void onProductChange() {
+        initPreviewer();
+    }
+
+    @Override
+    public void onResume() {
+        Log.e(TAG, "onResume");
+        super.onResume();
+        initPreviewer();
+        onProductChange();
+        if(mVideoSurface == null) {
+            Log.e(TAG, "mVideoSurface is null");
+        }
+    }
+
+    private void initPreviewer() {
+        DJIBaseProduct product = DroneXPApplication.getProductInstance();
+        if (product == null || !product.isConnected()) {
+            showToast(getString(R.string.disconnected));
+        } else {
+            if (null != mVideoSurface) {
+                mVideoSurface.setSurfaceTextureListener(this);
+            }
+            if (!product.getModel().equals(DJIBaseProduct.Model.UnknownAircraft))
+            {
+                DJICamera camera = product.getCamera();
+                if (camera != null){
+                    // Set the callback
+                    camera.setDJICameraReceivedVideoDataCallback(mReceivedVideoDataCallBack);
+                }
+            }
+        }
+    }
+    private void uninitPreviewer() {
+        DJICamera camera = DroneXPApplication.getCameraInstance();
+        if (camera != null){
+            // Reset the callback
+            DroneXPApplication.getCameraInstance().setDJICameraReceivedVideoDataCallback(null);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+    public void onReturn(View view){
+        this.finish();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Log.e(TAG, "onSurfaceTextureAvailable");
+        if (mCodecManager == null) {
+            mCodecManager = new DJICodecManager(this, surface, width, height);
+        }
+    }
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        Log.e(TAG, "onSurfaceTextureSizeChanged");
+    }
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        Log.e(TAG,"onSurfaceTextureDestroyed");
+        if (mCodecManager != null) {
+            mCodecManager.cleanSurface();
+            mCodecManager = null;
+        }
+        return false;
+    }
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+    }
+
+    public void showToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initUI() {
+        // init mVideoSurface
+        mVideoSurface = (TextureView)findViewById(R.id.video_previewer_surface);
+
+        if (null != mVideoSurface) {
+            mVideoSurface.setSurfaceTextureListener(this);
+        }
+    }
+
 }
