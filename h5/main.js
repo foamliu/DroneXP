010e743d19f01d9d738e55dafcf6e9502d9e1358
Yang Liu <foamliu@yeah.net>
2016-08-29 13:19:33 +0800

    创建首个流媒体页面

---
+++ h5/main.js
@@ -0,0 +1,8 @@
+
+navigator.webkitGetUserMedia({
+  video:true,
+  audio:true
+},function (stream) {
+  var video = document.querySelector('video');
+  video.src = window.URL.createObjectURL(stream);
+},function(err){});
