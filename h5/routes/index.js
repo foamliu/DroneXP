a01a7887f84e7c9075e077fe83005474389a24c5
刘杨 <liuyang@guanliyuandeMacBook-Air.local>
2016-10-08 17:18:24 +0800

    update

--- h5/routes/index.js
+++ h5/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '空间穿越' });
+  res.render('index', { title: '产品介绍' });
 });
 
 module.exports = router;
