510e48fb668b58fa110158199f6573142d6ce149
刘杨 <foamliu@yeah.net>
2016-10-26 20:19:19 +0800

    集成高德天气API，报天气

--- XiaoDaiRobot/app/src/main/java/cc/stargroup/xiaodai/MainActivity.java
+++ XiaoDaiRobot/app/src/main/java/cc/stargroup/xiaodai/MainActivity.java
@@ -3,31 +3,51 @@ package cc.stargroup.xiaodai;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.baidu.tts.auth.AuthInfo;
 import com.baidu.tts.client.SpeechError;
 import com.baidu.tts.client.SpeechSynthesizer;
 import com.baidu.tts.client.SpeechSynthesizerListener;
 import com.baidu.tts.client.TtsMode;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import static com.baidu.tts.client.SpeechSynthesizer.AUDIO_BITRATE_AMR_15K85;
 import static com.baidu.tts.client.SpeechSynthesizer.AUDIO_ENCODE_AMR;
 import static com.baidu.tts.client.SpeechSynthesizer.MIX_MODE_DEFAULT;
 
-public class MainActivity extends AppCompatActivity implements SpeechSynthesizerListener {
+public class MainActivity extends AppCompatActivity implements SpeechSynthesizerListener, View.OnClickListener {
     private static final String TAG = MainActivity.class.getSimpleName();
     private SpeechSynthesizer speechSynthesizer;
+
+    private Button mWeatherBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        init();
+        initUI();
+        initSynthesizer();
 
     }
 
-    private void init() {
+    private void initUI() {
+        mWeatherBtn = (Button) findViewById(R.id.btn_weather);
+        mWeatherBtn.setOnClickListener(this);
+    }
+
+    private void initSynthesizer() {
 
         speechSynthesizer = SpeechSynthesizer.getInstance();
         speechSynthesizer.setContext(this.getApplicationContext());
@@ -47,8 +67,6 @@ public class MainActivity extends AppCompatActivity implements SpeechSynthesizer
         speechSynthesizer.setParam(SpeechSynthesizer. PARAM_AUDIO_ENCODE, AUDIO_ENCODE_AMR);
         speechSynthesizer.setParam(SpeechSynthesizer. PARAM_AUDIO_RATE, AUDIO_BITRATE_AMR_15K85);
         speechSynthesizer.setParam(SpeechSynthesizer. PARAM_VOCODER_OPTIM_LEVEL, "0");
-
-        speechSynthesizer.speak("你好");
     }
 
     @Override
@@ -84,5 +102,71 @@ public class MainActivity extends AppCompatActivity implements SpeechSynthesizer
     @Override
     public void onError(String s, SpeechError speechError) {
         Log.d(TAG, "onError " + s + speechError.description);
+    }
+
+    public void showToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_weather: {
+                doWeather();
+
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    private void doWeather() {
+        String url = "http://restapi.amap.com/v3/weather/weatherInfo?city=310000&key=543cf533c1988351e6df98030eca10f2";
+        JsonObjectRequest jsonRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // the response is already constructed as a JSONObject!
+                        try {
+                            /*
+                            {"status":"1","count":"1","info":"OK","infocode":"10000",
+                            "lives":[{"province":"上海","city":"上海市","adcode":"310000","weather":"中雨","temperature":"19",
+                            "winddirection":"东北","windpower":"4","humidity":"94","reporttime":"2016-10-26 18:00:00"}]}
+                             */
+                            JSONArray lives = response.getJSONArray("lives");
+                            JSONObject result = lives.getJSONObject(0);
+                            String city = result.getString("city");
+                            String weather = result.getString("weather");
+                            String temperature = result.getString("temperature");
+                            String winddirection = result.getString("winddirection");
+                            String windpower = result.getString("windpower");
+                            String humidity = result.getString("humidity");
+
+                            StringBuilder sb = new StringBuilder();
+                            sb.append(city);
+                            sb.append("今天" + weather + ",");
+                            sb.append("平均" + temperature + "摄氏度" + ",");
+                            sb.append(winddirection + "风" + windpower + "级" + ".");
+
+                            speechSynthesizer.speak(sb.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(jsonRequest);
+
     }
 }
